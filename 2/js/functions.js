2331fe525d4761760cf3c1c5b0bda5798facb45a
Keks <keks@htmlacademy.ru>
2024-07-29 21:11:40 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,46 @@
+let checkLength = (string = '', maxLength = 1) => {
+
+    let result = string.length <= maxLength;
+
+    return result;
+
+}
+
+let checkPalindrome = (string = '') => {
+
+  string = string.replaceAll(" ", "");
+  let normalisedString = string.toUpperCase();
+  let placeholderString = "";
+
+  for (let i = normalisedString.length -1; i >= 0; i--){
+
+    let char = normalisedString[i];
+    placeholderString += char;
+  }
+
+
+  return normalisedString === placeholderString;
+}
+
+let extractingNumbers = (string = '') => {
+  string = string.toString();
+  let result = '';
+
+  for (let i = 0; i < string.length; i++) {
+      let char = string[i];
+
+      if (!Number.isNaN(parseInt(char))) {
+        result += char
+      }
+    }
+      let number = parseInt(result);
+
+      if (Number.isNaN(number)) {
+        return NaN;
+      }
+
+      return number;
+  }
+
+
+
