d63db29d8590494468aec6f095662c84c482c817
Keks <keks@htmlacademy.ru>
2024-08-19 00:23:48 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,46 +1,46 @@
-let checkLength = (string = '', maxLength = 1) => {
-
-    let result = string.length <= maxLength;
-
-    return result;
-
-}
-
-let checkPalindrome = (string = '') => {
-
-  string = string.replaceAll(" ", "");
-  let normalisedString = string.toUpperCase();
-  let placeholderString = "";
-
-  for (let i = normalisedString.length -1; i >= 0; i--){
-
-    let char = normalisedString[i];
-    placeholderString += char;
-  }
+const checkLength = (string = '', maxLength = 1) => string.length <= maxLength;
 
 
-  return normalisedString === placeholderString;
-}
+const checkPalindrome = (string = '') => {
+  string = string.replaceAll(' ', '').toLowerCase();
 
-let extractingNumbers = (string = '') => {
-  string = string.toString();
-  let result = '';
-
-  for (let i = 0; i < string.length; i++) {
-      let char = string[i];
-
-      if (!Number.isNaN(parseInt(char))) {
-        result += char
-      }
+  for (let i = 0; i < Math.floor(string.length / 2); i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false;
     }
-      let number = parseInt(result);
-
-      if (Number.isNaN(number)) {
-        return NaN;
-      }
-
-      return number;
   }
 
+  return true;
+};
+
+
+const extractingNumbers = (string = '') => {
+  string = string.toString();
+  const matched = string.match(/\d+/g);
+
+  return matched ? parseInt(matched.join(''), 10) : NaN;
+};
+
+// console.log('Тест первой функции');
+// console.log(checkLength('Hello', 2));
+// console.log(checkLength('Hello', 10));
+// console.log(checkLength('HElLO', 2));
+// console.log(checkLength('HeLLo', 10));
+
+// console.log('Тест второй функции');
+// console.log(checkPalindrome('топот'));
+// console.log(checkPalindrome('ДОвОд'));
+// console.log(checkPalindrome('Кекс'));
+// console.log(checkPalindrome('Лёша на полке клопа нашёл '));
+
+// console.log('Тест третьей функции');
+// console.log(extractingNumbers('2023 год'));
+// console.log(extractingNumbers('ECMAScript 2022'));
+// console.log(extractingNumbers('1 кефир, 0.5 батона'));
+// console.log(extractingNumbers('агент 007'));
+// console.log(extractingNumbers('а я томат'));
+// console.log(extractingNumbers(2023));
+// console.log(extractingNumbers(-1));
+// console.log(extractingNumbers(1.5));
 
 
